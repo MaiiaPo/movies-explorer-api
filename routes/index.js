27e026748045d64381ec2509cca4d2ede1e39a17
Maiia <maiiapo.com@gmail.com>
2023-07-18 19:25:09 +0500

    Доработка валидации.

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,14 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { celebrate } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const { signupValidation, signinValidation } = require('../utils/validation');
 
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+router.post('/signin', celebrate(signinValidation), login);
+router.post('/signup', celebrate(signupValidation), createUser);
 
 router.use(auth);
 router.use('/users', userRouter);
--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,15 @@
 const movieRoutes = require('express').Router();
 
+const { celebrate } = require('celebrate');
 const {
   getMovies,
   createMovie,
   deleteMovie,
 } = require('../controllers/movies');
+const { createMovieValidation, deleteMovieValidation } = require('../utils/validation');
 
 movieRoutes.get('/', getMovies);
-movieRoutes.post('/', createMovie);
-movieRoutes.delete('/:movieId', deleteMovie);
+movieRoutes.post('/', celebrate(createMovieValidation), createMovie);
+movieRoutes.delete('/:movieId', celebrate(deleteMovieValidation), deleteMovie);
 
 module.exports = movieRoutes;
