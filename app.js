dca97da5959bdde68a949ff96247b6f4553b3ff0
Maiia <maiiapo.com@gmail.com>
2023-07-18 18:20:07 +0500

    Ошибки + логирование

--- app.js
+++ app.js
@@ -2,19 +2,34 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 const router = require('./routes');
 const config = require('./config');
 
+const { errors } = require('celebrate');
+const errorsHandler = require('./middlewares/errors');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
+
 mongoose.connect(config.connectDbString);
 
-const { PORT = 3000 } = process.env;
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors);
+app.use(requestLogger);
 
 router.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.use((req, res, next) => next(new NotFoundError('Несуществующая страница')));
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorsHandler);
+
+app.listen(config.port, () => {
+  console.log(`App listening on port ${config.port}`);
 });
--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET, PORT } = process.env;
 
 const config = {
+  port: PORT || 3000,
   jwtSecret: NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
   connectDbString: 'mongodb://localhost:27017/bitfilmsdb',
 };
